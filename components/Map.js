4c0bbee07e5c76698ff8b46e2bf2192e19ac0906
hellskater <ksrinivasrao531@gmail.com>
2021-10-24 23:04:00 +0530

    day 3

---
+++ components/Map.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  //  Transform the search results object into the { latitude: 52.516272, longitude: 13.377722 } object
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  const center = getCenter(coordinates);
+
+  const [viewPort, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/srini53168/ckv5d1k3z26v615nz4ocuu1zn"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewPort}
+      onViewportChange={(nextViewPort) => setViewport(nextViewPort)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              aria-label="push-pin"
+            >
+              📌
+            </p>
+          </Marker>
+
+          {/* The popup that should show if we click on a Marker */}
+          {selectedLocation.long == result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoic3Jpbmk1MzE2OCIsImEiOiJja3Y1Y3Z5ZmMwb3h4Mm9wZzZvbWp0d2VrIn0.gB37tJEm3AJMrKzuKVQETw",
+  },
 };
